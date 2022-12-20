fc5fa3a288a410a2fd8f2dc08d108ada5e668c76
Damian Cyntler <damian@cyntler.com>
2022-12-20 17:48:31 +0100

    feat: add api fetching util

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,4 +1,4 @@
-import { API_URL } from '@/models';
+import { API_URL } from '@/consts';
 
 export const fetchApi = async (endpoint: string, method: string = 'GET') => {
   const result = await fetch(`${API_URL}${endpoint}`, {
