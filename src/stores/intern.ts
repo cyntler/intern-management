eba0cf418d0ed8b549e203d185fec78466fd6a8e
Damian Cyntler <damian@cyntler.com>
2022-12-20 17:49:06 +0100

    feat: add getIntentById func to intern store

--- src/stores/intern.ts
+++ src/stores/intern.ts
@@ -5,17 +5,24 @@ import { fetchApi } from '@/utils/fetchApi';
 import type { Intern, InternResponse } from '@/models';
 
 export const useInternStore = defineStore('intern', () => {
+  const isFetching = ref(true);
   const interns = ref<Intern[]>([]);
   const page = ref(1);
   const totalPages = ref(0);
 
   const fetchInterns = async () => {
-    const response = (await fetchApi('/users')) as InternResponse;
+    const response: InternResponse = await fetchApi(
+      `/users?page=${page.value}`,
+    );
 
+    isFetching.value = false;
     interns.value = response.data;
     page.value = response.page;
     totalPages.value = response.total_pages;
   };
 
-  return { interns, fetchInterns };
+  const getIntentById = (id: number) =>
+    interns.value.find((intent) => intent.id === id);
+
+  return { isFetching, interns, page, totalPages, fetchInterns, getIntentById };
 });
