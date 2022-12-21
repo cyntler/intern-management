c116520628635bbd23af897ffa42437bc14c0b2e
Damian Cyntler <damian@cyntler.com>
2022-12-21 08:48:13 +0100

    chore: ignore Pagination

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,7 @@ module.exports = {
     'vue/multi-word-component-names': [
       'error',
       {
-        ignores: ['Preloader', 'Button'],
+        ignores: ['Preloader', 'Button', 'Pagination'],
       },
     ],
   },
