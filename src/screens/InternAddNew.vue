9e9e19d3d60ca066b94c6d7a07c9ae68f66e3388
Damian Cyntler <damian@cyntler.com>
2022-12-21 08:51:18 +0100

    feat: add new intern screen

---
+++ src/screens/InternAddNew.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+import BackButton from '@/components/BackButton.vue';
+import InternForm from '@/components/InternForm.vue';
+import { useInternStore } from '@/stores/intern';
+
+const internStore = useInternStore();
+const router = useRouter();
+
+const handleSubmit = async (
+  firstName: string,
+  lastName: string,
+  avatar: string,
+) => {
+  await internStore.addIntern({
+    first_name: firstName,
+    last_name: lastName,
+    avatar,
+  });
+
+  router.push('/');
+};
+</script>
+
+<template>
+  <section class="intern-add-new">
+    <h1><BackButton /> Add intern</h1>
+    <h2>Complete the form below to add a new intern.</h2>
+    <InternForm @submit="handleSubmit" />
+  </section>
+</template>
