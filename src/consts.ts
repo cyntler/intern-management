198a02958622c05f8fcf087e7eeeb813857390ea
Damian Cyntler <damian@cyntler.com>
2022-12-21 09:06:52 +0100

    chore: fix scripts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 export const API_URL = 'https://reqres.in/api';
 export const INTERNS_PER_PAGE = 6;
-export const AVATAR_PLACEHOLDER_SRC = '/avatar-placeholder.png';
+export const AVATAR_PLACEHOLDER_SRC = `${
+  import.meta.env.PROD ? '/intern-management/' : ''
+}/avatar-placeholder.png`;
